b7e72b803a5dddb37589d7d1f05eab725f2d3e3e
yohei.hokari <yohei.hokari@gmail.com>
2025-08-21 22:05:59 +0900

    feat: length of tuple

---
+++ easy/LengthOfTuple/length_of_tuple.ts
@@ -0,0 +1,10 @@
+export {};
+
+type tesla = ['tesla', 'model 3', 'model X', 'model Y']
+type spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT']
+
+// indexed access types
+type Length<T extends readonly any[]> = T['length']
+
+type teslaLength = Length<tesla>  // expected 4
+type spaceXLength = Length<spaceX> // expected 5

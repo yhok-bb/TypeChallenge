b283b80b6851c1a311749b4486976f9f8dfb45cc
yohei.hokari <yohei.hokari@gmail.com>
2025-09-03 22:28:39 +0900

    feat: omit

---
+++ Omit/omit.ts
@@ -0,0 +1,13 @@
+type MyOmit<T, K extends keyof T> = { [P in keyof T as P extends K ? never : P]: T[P] }
+
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+type TodoPreview = MyOmit<Todo, 'description' | 'title'>
+
+const todo: TodoPreview = {
+  completed: false,
+}
